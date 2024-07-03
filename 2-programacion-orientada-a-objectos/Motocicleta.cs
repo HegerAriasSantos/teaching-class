fe333c1e5635a27eadbcc9921ca2785a16e7fbc0
HencelAriasSantos <81398653+HencelAriasSantos@users.noreply.github.com>
2024-07-03 09:20:47 -0400

    Tarea 2

---
+++ 2-programacion-orientada-a-objectos/Motocicleta.cs
@@ -0,0 +1,17 @@
+﻿namespace Tarea_2.Paso_2Herencia_y_Polimorfismo
+{
+    public class Motocicleta : Vehiculo
+    {
+        public int Canastas { get; set; }
+
+        public Motocicleta(string marca, string modelo, int año, int canastas)
+            : base(marca, modelo, año)
+        {
+            Canastas = canastas;
+        }
+        public override void MostrarInformacion()
+        {
+            Console.WriteLine($"Camioneta - Marca: {Marca}, Modelo: {Modelo}, Año: {Año}, Canastas: {Canastas}");
+        }
+    }
+}
