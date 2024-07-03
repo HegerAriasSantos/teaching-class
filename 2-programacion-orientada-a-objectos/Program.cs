a3d5e6a5c63c0a43e203b0921998eaf5daa6b2e4
HencelAriasSantos <81398653+HencelAriasSantos@users.noreply.github.com>
2024-07-03 07:28:56 -0400

    Tarea 2

---
+++ 2-programacion-orientada-a-objectos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+class Persona
+{
+    //atributos
+    public string nombre;
+    public int edad; 
+
+    //Contructor
+    public Persona (string nombre, int edad)
+    {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+        // metodo para moostrar informacion 
+        public void MostrarInformacion()
+{
+        Console.WriteLine($"Nombre: {nombre}, Edad: {edad}");
+}
+}
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        //crea una lista de personas
+        List<Persona> personas = new List<Persona>();
+        //crear una instancia de la clase persona usando el contructor
+        personas.Add(new Persona("Juan", 30));
+        personas.Add(new Persona("Pedro", 55));
+        personas.Add(new Persona("Maria", 62));
+        personas.Add(new Persona("Jose", 32));
+        personas.Add(new Persona("Juana", 34));
+
+        //llamar al metodo para mostrar informacion 
+        for (int i = 0; i < personas.Count; i++)
+        {
+            personas[i].MostrarInformacion();
+        }
+}
+}
